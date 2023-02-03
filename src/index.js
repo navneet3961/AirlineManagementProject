45e7c52907a361f688d4f2524c5ff3b922c344c6
Navneet Varshney <navarshney99@gmail.com>
2023-02-04 00:12:03 +0530

    City model created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const CityRepository = require("./repository/city-repository");
 
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
     const app = express();
-    app.listen(PORT, () => {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.listen(PORT, async () => {
         console.log(`Server has started at ${PORT}`);
+        const city = new CityRepository();
+        city.createCity({ name: "Noida" });
     });
 }
 
